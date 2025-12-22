a74c859e5dc50c7f1e5b89cf571a3f333e83b10b
AdhikariManish23 <manishadhikari1992@gmail.com>
2025-12-22 13:48:32 +0530

    up

---
+++ C++/Leetcode/Leetcode345.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// check the error
+// you have to
+
+bool isVowel(char key)
+{
+    string vowel = "aeiouAEIOU";
+    for (auto it : vowel)
+    {
+        if (it == key)
+            return true;
+    }
+    return false;
+}
+
+string reverseVowels(string s)
+{
+    int i = 0;
+    int j = s.length() - 1;
+    while (i < j)
+    {
+        while (i < j && !isVowel(s[i]))
+            i++;
+        while (i < j && !isVowel(s[j]))
+            j--;
+
+        swap(s[i], s[j]);
+        i++;
+        j--;
+    }
+    return s;
+}
+
+int main()
+{
+    string str = "Leetcode";
+    cout << reverseVowels(str);
+}
